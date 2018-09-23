1d09dfb54751bb2955f9f3bcba7d701384ebf287
rezamike <michael.sanaiha@gmail.com>
2018-09-23 13:41:02 -0700

    play with update - not sure

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 let inquirer = require("inquirer");
 let request = require("request");
 let mysql = require("mysql");
+let chalk = require("chalk");
 
 let connection = mysql.createConnection({
     host: "LocalHost",
@@ -13,48 +14,101 @@ let connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
 
-    showItems();
-    var t = setTimeout(function(){ buyItem() }, 2000);
+    inquirer
+        .prompt({
+            name: "main",
+            type: "list",
+            choices: ["View and Purchase Items", "Creator", "Quit"],
+            message: chalk.cyan.bold("\nWelcome to the Film Prop Market (FPM)!\nMake a selection below....\n")
+        }).then(function (response) {
+
+            switch (response.main) {
+                case "View and Purchase Items":
+                    showItems();
+                    var t = setTimeout(function () { selectItem() }, 2000);
+
+                    break;
+                case "Creator":
+
+
+                    break;
+                case "Quit":
+                    connection.end();
+                    break;
+            }
+        });
+
+
+
     // createProduct();
 });
 
 // function 
 
-function buyItem() {
+function selectItem() {
     inquirer
         .prompt({
             name: "main",
             type: "input",
-            message: "\nWhich product would you like to buy (choose by number)?",
+            message: chalk.cyan.bold("\nWhich product would you like to buy (choose by number)?"),
         })
         .then(function (answer) {
-            connection.query("SELECT * FROM products ORDER BY item_id", function (err, res) {
+            connection.query("SELECT * FROM products", function (err, res) {
                 if (err) throw err;
 
-                console.log("-------------------------------------------------------------\n");
-                
+                console.log(chalk.cyan.bold("-------------------------------------------------------------\n"));
                 for (let i = 0; i < res.length; i++) {
-                    let x = res[i].item_id;
-                    if (x === answer.main) {
-                        console.log("balls");
-                        // console.log(this.item_id + ". " + this.product_name + " - " + this.description + "  $" + this.price + "\n");
-                        // console.log("There are " + this.stock_quantity + " in stock.");
+                    if (parseInt(res[i].item_id) === parseInt(answer.main)) {
+                        console.log(chalk.gray(res[i].item_id) + ". " + chalk.green(res[i].product_name) + " - " + chalk.italic(res[i].description) + chalk.red("  $") + chalk.red(res[i].price) + "\n");
+                        console.log(chalk.inverse.bold.italic("There are currently " + res[i].stock_quantity + " in stock."));
                     }
                 }
-                console.log("\n-------------------------------------------------------------\n");
+                console.log(chalk.cyan.bold("\n-------------------------------------------------------------\n"));
+
+                inquirer
+                //     .prompt({
+                //         name: "notMain",
+                //         type: "input",
+                //         message: chalk.cyan.bold("\nHow many would you like to purchase?"),
+                //     })
+                //     .then(function (other) {
+                //         connection.query("UPDATE products SET ? WHERE ?", [
+                //             {
+                //                 stock_quantity: stock_quantity - other.notMain
+                //             },
+                //             {
+                //                 item_id: answer.main
+                //             }
+                //         ], function (err, res) {
+                //             if (err) throw err;
+
+                //             console.log(chalk.cyan.bold("-------------------------------------------------------------\n"));
+                //             for (let i = 0; i < res.length; i++) {
+                //                 if (parseInt(res[i].item_id) === parseInt(answer.main)) {
+                //                     console.log(chalk.gray(res[i].item_id) + ". " + chalk.green(res[i].product_name) + " - " + chalk.italic(res[i].description) + chalk.red("  $") + chalk.red(res[i].price) + "\n");
+                //                     console.log(chalk.inverse.bold.italic("There are currently " + res[i].stock_quantity + " in stock."));
+                //                 }
+                //             }
+                //             console.log(chalk.cyan.bold("\n-------------------------------------------------------------\n"));
+
+                //             // buy function(update)
+
+                //             // menu menu
+                //         });
+                //     });
             });
         });
 };
 
 function showItems() {
-    console.log("\nShowing all available products...");
-    console.log("-------------------------------------------------------------\n");
+    console.log(chalk.cyan.bold("\nShowing all available products..."));
+    console.log(chalk.cyan.bold("-------------------------------------------------------------\n"));
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
 
         for (let i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + ". " + res[i].product_name + " - " + res[i].description + "  $" + res[i].price);
+            console.log(chalk.gray(res[i].item_id) + ". " + chalk.green(res[i].product_name) + " - " + chalk.italic(res[i].description) + chalk.red("  $") + chalk.red(res[i].price));
         }
-        console.log("\n-------------------------------------------------------------\n");
+        console.log(chalk.cyan.bold("\n-------------------------------------------------------------\n"));
     });
-}
+};
